534bbcaad3e34bf9d979f01dc90332dc16940d82
Johanes Indra Pradana Pao <financiallover@gmail.com>
2016-08-22 17:37:58 +0700

    Initial Commit & First Phase
    Basic design configuration, basic navbar.
    Figure out how to create sidebar navigation just like in simpliq....

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/* HEADER */
+require_once('inklusi/kepalaDiKaki.ink.php');
+echo $kepala;
+?>
+
+<!-- KONTEN -->
+<nav class='navbar navbar-fixed-top navbar-dark'>
+    <a class='navbar-brand col-lg-2 col-sm-1 col-xs-12' href='index.php'>
+        <span>Filavest!</span>
+    </a>
+</nav>
+
+<?php
+/* FOOTER */
+echo $kaki;
+?>
